2b1537a2b4aec5cd435f4f82798e90a1491e3aaf
Jitendra <chavhanj2022@gmail.com>
2023-09-24 19:44:28 +0530

    display all user on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @users = User.limit(10).order('created_at DESC')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  PROFILE_TITLE = [
+    "Sr RoR Developer",
+    "Jr RoR Developer",
+    "RoR Developer",
+    "Full Stack RoR Developer",
+    "Jr Full Stack Developer",
+    "Sr Full Stack Developer"
+  ].freeze
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
