66b2aab0f23c23f557214347347c269db23a113c
Jitendra <chavhanj2022@gmail.com>
2023-09-24 20:45:41 +0530

    create profile link

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,10 @@ class User < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def address
+    "#{city}, #{state}, #{country}, #{pincode}"
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     ["about", "city", "contact_no", "country", "created_at", "current_sign_in_at", "current_sign_in_ip", "dob", "email", "encrypted_password", "first_name", "id", "last_name", "last_sign_in_at", "last_sign_in_ip", "pincode", "profile_title", "remember_created_at", "reset_password_sent_at", "reset_password_token", "sign_in_count", "state", "street_address", "updated_at", "username"]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'home/index'
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'member/:id', to: 'members#show', as: 'member' 
 end
