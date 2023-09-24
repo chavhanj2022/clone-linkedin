3008c2fb2074eb044b1f3959f0932b4814e65690
Jitendra <chavhanj2022@gmail.com>
2023-09-24 20:07:00 +0530

    add ransack search for user on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @users = User.limit(10).order('created_at DESC')
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true).limit(10).order('created_at DESC')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,12 @@ class User < ApplicationRecord
   def name
     "#{first_name} #{last_name}"
   end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["about", "city", "contact_no", "country", "created_at", "current_sign_in_at", "current_sign_in_ip", "dob", "email", "encrypted_password", "first_name", "id", "last_name", "last_sign_in_at", "last_sign_in_ip", "pincode", "profile_title", "remember_created_at", "reset_password_sent_at", "reset_password_token", "sign_in_count", "state", "street_address", "updated_at", "username"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end
