4af7e7167b6b7ff29bc2cbdf09dca5cbe92e59f3
Jitendra <chavhanj2022@gmail.com>
2023-10-04 19:09:00 +0530

    Save work experience in the database and updating DOM with turbo-stream

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -6,7 +6,16 @@ class WorkExperiencesController < ApplicationController
         @work_experience = current_user.work_experiences.new
     end
 
-    def create; end
+    def create
+        @work_experience = current_user.work_experiences.new(work_experience_params)
+        respond_to do |format|
+            if @work_experience.save
+                format.turbo_stream { render turbo_stream: turbo_stream.append('work_experience_items', partial: 'work_experiences/work_experience', locals: {work_experience: @work_experience}) }
+            else
+                format.turbo_stream { render turbo_stream: turbo_stream.replace('remote_modal', partial: 'shared/turbo_model', locals: {form_partial: 'work_experiences/form', modal_title: 'Add new work experience'})}
+            end
+        end
+    end
 
     def edit; end
 
@@ -20,6 +29,6 @@ class WorkExperiencesController < ApplicationController
     end
 
     def work_experience_params
-        params.require(:work_experience).permit(:start_date, :currently_working_here, :company, :employment_type, :location, :location_type, :description, :user_id, :job_title)
+        params.require(:work_experience).permit(:start_date, :currently_working_here, :company, :employment_type, :location, :location_type, :description, :user_id, :job_title,:end_date)
     end
 end
