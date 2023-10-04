52211d1f9e3bb5dc84371e1d67a627e47b35c074
Jitendra <chavhanj2022@gmail.com>
2023-10-04 05:50:23 +0530

    add work experience modal

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   patch 'update_description', to: 'members#update_description', as: 'update_member_description'
   get 'edit_profile', to: 'members#edit_profile', as: 'edit_profile'
   patch 'update_profile', to: 'members#update_profile', as: 'update_profile'
+  
+  resources :work_experiences
 
 end
