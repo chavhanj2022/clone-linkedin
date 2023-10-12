331505913cad6a272bfa7fcc9a6ecc2058c4ca46
Jitendra <chavhanj2022@gmail.com>
2023-10-12 09:22:38 +0530

    recieved and requested connection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def status_of_user_connection_status(current_user, user)
+        return nil if current_user == user
+        
+        current_user.connections.find_by(connected_user_id: user.id)&.status
+    end
 end
--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,6 +1,8 @@
 class Connection < ApplicationRecord
   belongs_to :user
 
+  belongs_to :requested, foreign_key: :connected_user_id, class_name: 'User'
+  belongs_to :received,  foreign_key: :user_id, class_name: 'User'
   #validations
   validates :connected_user_id, presence: true
   validates :status, presence: true, inclusion: { in: %w(pending accepted rejected deleted)}
